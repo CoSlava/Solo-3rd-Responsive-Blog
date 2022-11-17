7e7974b66b5f298c235c6d9d8e86ba8895092bbf
CoSlava <vecojocari@gmail.com>
2022-11-17 16:19:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function removeHidden() {
+    document.getElementById("blog-container-one").classList.remove("hidden")
+    document.getElementById("blog-container-two").classList.remove("hidden")
+    document.getElementById("blog-container-three").classList.remove("hidden")
+    document.getElementById("view-more").classList.add("hidden")
+}
